fa548ffe6ae65199519b73b95411e17b61dbfb35
Kerem Bayülgen <kerembayulgen@outlook.com>
2025-09-28 21:54:28 +0200

    feat: add additional support

--- src/logic/profile.ts
+++ src/logic/profile.ts
@@ -1,23 +1,53 @@
 import { getPreferenceValues } from "@vicinae/api"
-import { readdirSync, existsSync } from "fs"
+import { existsSync, readdirSync } from "fs"
 import * as os from "os"
 
+// Common name, organization name, product name
+function getBrowserFolder(): [string, string, string] {
+  const browser = getPreferenceValues()["browser"]
+  switch (browser) {
+    case "Firefox":
+      return ["org.mozilla.firefox", ".mozilla/firefox", "firefox"]
+    case "Zen":
+      return ["app.zen_browser.zen", ".zen", "zen"]
+    case "Mullvad":
+      return ["net.mullvad.MullvadBrowser", ".mullvad-browser", "mullvad"]
+    case "Librewolf":
+      return ["io.gitlab.librewolf-community", ".librewolf", "librewolf"]
+    case "Floorp":
+      return ["one.ablaze.floorp", ".floorp", "floorp"]
+    case "IceCat":
+      return ["", ".mozilla/icecat", "icecat"]
+  }
+  return ["", "", ""]
+}
+
 function getMozillaFolder(): string {
-  const install_method = getPreferenceValues()
+  const browser_folder = getBrowserFolder()
+  const prefs = getPreferenceValues()
   const username = os.userInfo().username
-  switch (install_method["install-method"]) {
+  if (prefs["install_method"] === "Flatpak" && prefs["browser"] == "IceCat") {
+    throw Error("GNU IceCat does not have a flatpak version!")
+  }
+  switch (prefs["install-method"]) {
     case "Flatpak":
-      return `/home/${username}/.var/app/org.mozilla.firefox/.mozilla/firefox/`
+      return `/home/${username}/.var/app/${browser_folder[0]}/${browser_folder[1]}`
     case "System":
-      return `/home/${username}/.mozilla/firefox/`
+      return `/home/${username}/${browser_folder[1]}/`
     case "Snap":
-      return `/home/${username}/snap/firefox/common/.mozilla/firefox/`
+      return `/home/${username}/snap/${browser_folder[2]}/common/${browser_folder[1]}/`
   }
   return ""
 }
 
 export function getValidProfile(): string {
   const mozilla_folder = getMozillaFolder()
+  let exists = existsSync(mozilla_folder)
+  if (!exists) {
+    throw new Error(
+      `Selected configuration not found! Did you specify your installation properly? Check if this folder exists: ${mozilla_folder}`,
+    )
+  }
   const override_profile: string = getPreferenceValues()["profile"]
   if (override_profile) {
     return `${mozilla_folder}/${override_profile}`
